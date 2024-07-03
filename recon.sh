db53d4d478fe482e4440082d4ff13f482c1230e5
FedeLu01 <fedelucero95@gmail.com>
2024-07-03 01:14:21 -0300

    fixes to recon.sh

--- recon.sh
+++ recon.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 if [[ -d $DOMAIN ]]; then
+    mkdir $DOMAIN && cd $DOMAIN && touch "$DOMAIN"_resolvedDomains.txt && touch "$DOMAIN"_webservers.txt && touch "$DOMAIN"_openports.txt 
 
     while true; do
         echo "Recon for $DOMAIN:"
-        mkdir $DOMAIN | domain-recon -d $DOMAIN --domains-only | dnsx -silent > "$DOMAIN\_resolvedDomains.txt" | anew -q "$DOMAIN\_resolvedDomains.txt"
-        httpx -l "resolvedDomains.txt" -t 10 -silent > "$DOMAIN\_webservers.txt" | anew "$DOMAIN\_webservers.txt" | notify -silent -bulk
-        smap -iL "$DOMAIN\_resolvedDomains.txt" | anew "$DOMAIN\_openports.txt" 
+        mkdir $DOMAIN | domain-recon -d $DOMAIN --domains-only | dnsx -silent > "$DOMAIN"_resolvedDomains.txt | anew -q "$DOMAIN"_resolvedDomains.txt
+        httpx -l "$DOMAIN"_resolvedDomains.txt -t 10 -silent > "$DOMAIN"_webservers.txt | anew "$DOMAIN"_webservers.txt | notify -silent -bulk
+        smap -iL "$DOMAIN"_resolvedDomains.txt | anew "$DOMAIN"_openports.txt 
     done
 else
     echo "error" | notify -silent -bulk
